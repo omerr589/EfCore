2079f5178461286695f4923fcfed82f02442dfbd
omer589 <omerr589@gmail.com>
2024-06-24 22:21:31 +0300

    Scaffold

--- EfCore.DbFirst.Scaffold/Program.cs
+++ EfCore.DbFirst.Scaffold/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("sa");
+﻿using EfCore.DbFirst.Scaffold.Models;
+using Microsoft.EntityFrameworkCore;
+
+using (var dbContext = new EfCoreDbFirstContext())
+{
+    var procuts = await dbContext.Products.ToListAsync();
+
+    procuts.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} - Name: {p.Name} Price: {p.Price} Stock: {p.Stock}");
+    });
+}
